d72d2a82fec457f5dedb5edd1f55bc37a99cde9e
Yuchen Ouyang <ouyangyuchen619@gmail.com>
2025-03-31 23:22:02 +0200

    Remove helper functions for tuple type

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,7 +2,6 @@
 #define UTILS_HPP
 
 #include <cstdint>
-#include <tuple>
 
 namespace stream {
 using TsType = int64_t;  // timestamp type
@@ -13,43 +12,6 @@ struct TupleType {
   KeyType key_;
   ValueType value_;
 };
-
-// helper functions to access tuple elements
-template <typename KeyType, typename ValueType>
-static auto GetTimestamp(const TupleType<KeyType, ValueType> &tuple) -> TsType {
-  return tuple.timestamp_;
-}
-
-template <typename KeyType, typename ValueType>
-static auto GetKey(const TupleType<KeyType, ValueType> &tuple) -> KeyType {
-  return tuple.key_;
-}
-
-template <typename KeyType, typename ValueType>
-static auto GetValue(const TupleType<KeyType, ValueType> &tuple) -> ValueType {
-  return tuple.value_;
-}
-
-template <typename KeyType, typename ValueType>
-static auto GetTimestamp(TupleType<KeyType, ValueType> &tuple) -> TsType & {
-  return tuple.timestamp_;
-}
-
-template <typename KeyType, typename ValueType>
-static auto GetKey(TupleType<KeyType, ValueType> &tuple) -> KeyType & {
-  return tuple.key_;
-}
-
-template <typename KeyType, typename ValueType>
-static auto GetValue(TupleType<KeyType, ValueType> &tuple) -> ValueType & {
-  return tuple.value_;
-}
-
-template <typename KeyType, typename ValueType>
-static auto MakeTuple(const TsType &timestamp, const KeyType &key, const ValueType &value)
-    -> TupleType<KeyType, ValueType> {
-  return {timestamp, key, value};
-}
 
 };  // namespace stream
 
