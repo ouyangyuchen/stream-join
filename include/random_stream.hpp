6bcc5a61a572805bf23660a7e172fc4adcb80c0d
Yuchen Ouyang <ouyangyuchen619@gmail.com>
2025-04-01 12:03:10 +0200

    Revert "Separate definitions behind the declarations"
    This reverts commit 72b58ba0d4f8f8219146e2919f62221929e89109.
    Because use out-of-line definitions in the non-template class will incur duplicate symbol error during the linking.

--- include/random_stream.hpp
+++ include/random_stream.hpp
@@ -10,15 +10,26 @@ namespace stream {
 // RandomStream generates random tuples with a specified key range and timestamp range
 class RandomStream : public Stream<int64_t, int64_t> {
  public:
-  RandomStream(TsType end_timestamp, const std::pair<int64_t, int64_t> &key_range);
+  RandomStream(const TsType end_timestamp, const std::pair<int64_t, int64_t> &key_range)
+      : end_timestamp_(end_timestamp), key_range_(key_range), generator_(std::random_device()()) {
+    if (key_range_.first > key_range_.second) {
+      throw std::invalid_argument("Invalid key range");
+    }
+  }
 
-  ~RandomStream() override = default;
+  auto operator>>(TupleType<int64_t, int64_t> &tuple) -> RandomStream & override {
+    if (this->Eof()) {
+      throw std::runtime_error("End of stream reached");
+    }
+    // generate a random number within the key range
+    int64_t key = generator_() % (key_range_.second - key_range_.first + 1) + key_range_.first;
+    tuple = {start_timestamp_++, key, key};
+    return *this;
+  }
 
-  auto operator>>(TupleType<int64_t, int64_t> &tuple) -> RandomStream & override;
+  auto Available() -> bool override { return start_timestamp_ < end_timestamp_; }
 
-  auto Available() -> bool override;
-
-  auto Eof() -> bool override;
+  auto Eof() -> bool override { return start_timestamp_ >= end_timestamp_; }
 
  private:
   TsType start_timestamp_{};
@@ -28,28 +39,5 @@ class RandomStream : public Stream<int64_t, int64_t> {
 };
 
 }  // namespace stream
-
-stream::RandomStream::RandomStream(const TsType end_timestamp,
-                                   const std::pair<int64_t, int64_t> &key_range)
-    : end_timestamp_(end_timestamp), key_range_(key_range), generator_(std::random_device()()) {
-  if (key_range_.first > key_range_.second) {
-    throw std::invalid_argument("Invalid key range");
-  }
-}
-
-auto stream::RandomStream::operator>>(TupleType<int64_t, int64_t> &tuple) -> RandomStream & {
-  if (this->Eof()) {
-    throw std::runtime_error("End of stream reached");
-  }
-  // generate a random number within the key range
-  int64_t key = generator_() % (key_range_.second - key_range_.first + 1) + key_range_.first;
-  tuple = {start_timestamp_, key, key};
-  start_timestamp_++;
-  return *this;
-}
-
-auto stream::RandomStream::Available() -> bool { return start_timestamp_ < end_timestamp_; }
-
-auto stream::RandomStream::Eof() -> bool { return start_timestamp_ >= end_timestamp_; }
 
 #endif  // RANDOM_STREAM_HPP_
