16213df4b0588bf26f37067b514f1081b9d93b82
Yuchen Ouyang <ouyangyuchen619@gmail.com>
2025-04-10 12:35:29 +0200

    Refactor: generate at various rate for SequentialStream

--- include/stream/sequential_stream.hpp
+++ include/stream/sequential_stream.hpp
@@ -5,7 +5,12 @@ namespace stream {
 
 class SequentialStream : public Stream<int64_t, int64_t> {
  public:
-  SequentialStream(int64_t start, int64_t end) : start_(start), end_(end) {}
+  SequentialStream(int64_t start, int64_t end, int64_t step = 1)
+      : start_(start), end_(end), step_(step) {
+    if (step <= 0) {
+      throw std::invalid_argument("step must be greater than 0");
+    }
+  }
 
   auto Available() -> bool override { return start_ < end_; }
 
@@ -15,13 +20,14 @@ class SequentialStream : public Stream<int64_t, int64_t> {
     tuple.timestamp_ = start_;
     tuple.key_ = start_;
     tuple.value_ = start_;
-    ++start_;
+    start_ += step_;
     return *this;
   }
 
  private:
   int64_t start_;
   int64_t end_;
+  int64_t step_;
 };
 
 }  // namespace stream
