0e08e86444ee4148a905ba75a0d2907e2b56e23c
Yuchen Ouyang <ouyangyuchen619@gmail.com>
2025-04-14 12:57:44 +0200

    Use spdlog library for logging/debug

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,14 @@
 #include <gtest/gtest.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
 auto main(int argc, char **argv) -> int {
+  // Set up spdlog to use color output
+  auto console_logger = spdlog::stdout_color_mt("console");
+  console_logger->set_level(spdlog::level::debug);  // Set the log level to debug
+
+  spdlog::info("Starting tests...");
+
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
