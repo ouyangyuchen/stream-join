b4fbe2657d76091733cdc512c7b252d23d0747ad
Yuchen <ouyangyuchen619@gmail.com>
2025-05-14 12:05:56 +0000

    Remove useless rubbish tail tuples

--- include/stream/sequential_stream.hpp
+++ include/stream/sequential_stream.hpp
@@ -6,17 +6,15 @@ namespace stream {
 
 class SequentialStream : public Stream<int64_t, int64_t> {
  public:
-  SequentialStream(int64_t start, int64_t end, int64_t step = 1, size_t tail_rubbish_tuples = 0,
-                   int64_t rubbish_key = -0x1000000000)
-      : start_(start), end_(end), step_(step), tail_rubbish_tuples_(tail_rubbish_tuples), rubbish_key_(rubbish_key) {
+  SequentialStream(int64_t start, int64_t end, int64_t step = 1) : start_(start), end_(end), step_(step) {
     if (step <= 0) {
       throw std::invalid_argument("step must be greater than 0");
     }
   }
 
-  auto Available() -> bool override { return start_ < end_ || tail_rubbish_tuples_ > 0; }
+  auto Available() -> bool override { return start_ < end_; }
 
-  auto Eof() -> bool override { return start_ >= end_ && tail_rubbish_tuples_ == 0; }
+  auto Eof() -> bool override { return start_ >= end_; }
 
   auto operator>>(TupleType<int64_t, int64_t> &tuple) -> Stream<int64_t, int64_t> & override {
     if (start_ < end_) {
@@ -25,14 +23,7 @@ class SequentialStream : public Stream<int64_t, int64_t> {
       tuple.value_ = start_;
       start_ += step_;
     } else {
-      if (tail_rubbish_tuples_ > 0) {
-        tuple.timestamp_ = start_++;
-        tuple.key_ = rubbish_key_++;  // avoid duplicate key to fail the insertion of index
-        tuple.value_ = tuple.key_;
-        --tail_rubbish_tuples_;
-      } else {
-        throw std::runtime_error("No more tuples available");
-      }
+      throw std::runtime_error("End of stream reached");
     }
     return *this;
   }
@@ -41,8 +32,6 @@ class SequentialStream : public Stream<int64_t, int64_t> {
   int64_t start_;
   int64_t end_;
   int64_t step_;
-  size_t tail_rubbish_tuples_;
-  int64_t rubbish_key_;
 };
 
 }  // namespace stream
